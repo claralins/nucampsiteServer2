988c3e8b8e5b85b0949503ea8742e2b44d5c192f
Clara Lins <claralinsgomes@gmail.com>
2022-07-01 23:36:03 -0400

    mongoose population

--- models/user.js
+++ models/user.js
@@ -3,20 +3,20 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
-  // username: {
-  //   type: String,
-  //   required: true,
-  //   unique: true,
-  // },
-  // password: {
-  //   type: String,
-  //   required: true,
-  // },
+  firstname: {
+    type: String,
+    default: "",
+  },
+  lastname: {
+    type: String,
+    default: "",
+  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
+
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
